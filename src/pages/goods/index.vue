58dbcfe1ad3605053c53b70a69b6b3abea6fed0c
goodliang <swl8806@126.com>
2018-08-24 17:56:08 +0800

    no message

---
+++ src/pages/goods/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    {{motto}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      motto: '商品详情页'
+    }
+  },
+
+  components: {
+
+  },
+
+  methods: {
+
+  },
+
+  created() {
+
+  }
+}
+
+</script>
+<style scoped>
+
+
+</style>
