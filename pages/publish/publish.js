e60a4907d8397eec5e65dff44ac130a787eb477c
yj <yaojiao@juewei.com>
2018-08-27 15:01:00 +0800

    发布页

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -27,10 +27,8 @@ Page({
     isGave:0,
     address:0,
     deliver:true,
-    animal:'动物',
-    disabled:false,
-    checked:true
-  },
+    morePic:false
+      },
   onLoad:function(){
    
   },
@@ -51,8 +49,12 @@ Page({
       }
     })
   },
-  handleAnimalChange:function(){
-    
+  handleAnimalChange:function(event){
+
+    const detail = event.detail;
+        this.setData({
+            'morePic' : detail.value
+        })
   },
   navigateToAddress: function () {
     wx.navigateTo({
