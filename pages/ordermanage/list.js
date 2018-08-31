2cafa34d12d38914633e4caf689cf52869373d57
shuxingxu <905336308@qq.com>
2018-08-31 13:08:53 +0800

    confirm

--- pages/ordermanage/list.js
+++ pages/ordermanage/list.js
@@ -3,6 +3,9 @@ Page({
     current: "tab1",
     visible: false,
     alertMsg: "确定执行？",
+    eidePriceVisible: false,
+    searchContent:"",
+    deleteShow: true
 
   },
   onReady: function (e) {
@@ -10,9 +13,23 @@ Page({
 
 
   },
+  searchInput(e){
+    this.deleteShow = e.detail.value.length > 0 ? false : true;
+    this.searchContent = this.trim(e.detail.value);
+    this.setData({
+      deleteShow: this.deleteShow
+    })
+  },
+  deleteSearchContent(){
+    this.searchContent = "";
+    this.setData({
+      searchContent: this.searchContent,
+      deleteShow: true
+    })
+  },
 
-  tapTo({target}) {
-    let url = '/pages/publish/publish';
+  toConfirm({target}){
+    let url = '/pages/orderconfirm/details';
     if (target.dataset.id) url = url + '?id=' + target.dataset.id;
     wx.navigateTo({
       url: url,
@@ -39,6 +56,19 @@ Page({
       visible: false
     });
   },
+  openPriceAlert(){
+    this.setData({
+      eidePriceVisible: true
+    })
+  },
+  closePriceAlert(){
+    this.setData({
+      eidePriceVisible: false
+    })
+  },
+  trim(str){ 
+    return str.replace(/(^\s*)|(\s*$)/g, ""); 
+  }
 
 
 })
