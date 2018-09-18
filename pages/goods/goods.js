e9bbb9b8e79b3430a21864c08f6e4d72b043aa66
goodliang <swl8806@126.com>
2018-09-18 16:25:44 +0800

    no message

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -3,54 +3,50 @@
 const app = getApp()
 
 Page({
-  data: {
-    imgUrls: [
-      'https://j-image.missfresh.cn/img_20180625145444571.jpg?iopcmd=thumbnail&type=4&width=640',
-      'https://j-image.missfresh.cn/img_20180625145444571.jpg?iopcmd=thumbnail&type=4&width=640',
-      'https://j-image.missfresh.cn/img_20180625145444571.jpg?iopcmd=thumbnail&type=4&width=640'
-    ],
-    goods:{}
-  },
-  onLoad:function(option){
+    data: {
+        imgUrls: [
+            'https://j-image.missfresh.cn/img_20180625145444571.jpg?iopcmd=thumbnail&type=4&width=640',
+            'https://j-image.missfresh.cn/img_20180625145444571.jpg?iopcmd=thumbnail&type=4&width=640',
+            'https://j-image.missfresh.cn/img_20180625145444571.jpg?iopcmd=thumbnail&type=4&width=640'
+        ],
+        goods: {}
+    },
+    onLoad: function(option) {
 
-          wx.request({
-           url: 'https://www.daohangwa.com/api//goods/get_goods_info',
-              data: {
-
-                goods_id:option.goods_id
-
-              },
-              success:  (res) =>{
-                if(res.data.code == 0){
-
-                  console.log(res.data.data.goods)
-
-                  this.setData({
-                    goods:res.data.data.goods
-                  })
+        wx.request({
+            url: 'https://www.daohangwa.com/api//goods/get_goods_info',
+            data: {
+                goods_id: 24
+            },
+            success: (res) => {
+                if (res.data.code == 0) {
+                    console.log(res.data.data.goods)
+                    this.setData({
+                        goods: res.data.data.goods
+                    })
                 }
-              }
-      })
+            }
+        })
 
 
 
 
 
-  },
-  homepage(){
-    wx.navigateTo({
-      url: '../home/index'
-    })
-  },
-  userpage() {
-    console.log('userpage')
-    wx.navigateTo({
-      url: '../ucenter/ucenter'
-    })
-  },
-  buy() {
-    wx.navigateTo({
-      url: '../order/index'
-    })
-  }
+    },
+    homepage() {
+        wx.navigateTo({
+            url: '../home/index'
+        })
+    },
+    userpage() {
+        console.log('userpage')
+        wx.navigateTo({
+            url: '../ucenter/ucenter'
+        })
+    },
+    buy() {
+        wx.navigateTo({
+            url: '../order/index'
+        })
+    }
 })
