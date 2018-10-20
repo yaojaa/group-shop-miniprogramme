382db0cf6011c8384e40553225c5e23ffa4c5f81
shuxing <905336308@qq.com>
2018-10-20 21:17:09 +0800

    map

--- pages/map/index.js
+++ pages/map/index.js
@@ -9,12 +9,15 @@ Page({
     newAddress: [],
     oldAddress: [],
     openLocation: true,
+    buyType:1,
 
   },
-  onLoad: function () {
+  onLoad: function (e) {
     let _this = this;
     // this.openLocation(this);
     // 
+    this.data.buyType = e.delivery_method;
+
     wx.getStorage({
       key: 'historyAddress',
       success: function(res) {
@@ -87,7 +90,17 @@ Page({
   moveAddress(e){
     let data = e.currentTarget.dataset;
     let _this = this;
-    this.data.newAddress.unshift(this.data.oldAddress.splice(this.getIndex(this.data.oldAddress, data.id), 1)[0]); //删除历史纪录展示并加入新地址
+    if(this.data.newAddress[0]){
+      this.data.oldAddress.unshift(this.data.newAddress[0]);
+    }
+
+    // this.data.newAddress.unshift(this.data.oldAddress.splice(this.getIndex(this.data.oldAddress, data.id), 1)[0]); //删除历史纪录展示并加入新地址
+
+    this.data.newAddress = this.data.oldAddress.splice(this.getIndex(this.data.oldAddress, data.id), 1);
+    
+
+    console.log(this.data.oldAddress,this.newAddress)
+
     wx.showToast({ title: "添加成功" })
     _this.setData({
       oldAddress: _this.data.oldAddress,
@@ -108,7 +121,6 @@ Page({
       newAddress: _this.data.newAddress
     })
 
-    console.log(this.data)
   },
 
   submit() {
@@ -116,12 +128,10 @@ Page({
       wx.showToast({ title: "请先添加地址", icon: "none" })
       return;
     }
-    if (!this.data.newAddress[0].details) {
+    if (!this.data.newAddress[0].details && this.data.buyType == 1) {
       wx.showToast({ title: "请填写详细地址", icon: "none" })
       return;
     }
-
-
 
     wx.navigateBack({
       delta: 1
@@ -146,13 +156,23 @@ Page({
           success(e){
             if(!e.name || !e.address) return;
 
-            _this.data.newAddress.unshift({
+            _this.data.oldAddress = _this.data.newAddress.concat(_this.data.oldAddress);
+
+            // _this.data.newAddress.unshift({
+            //   id: new Date().getTime(),
+            //   name: e.name,
+            //   address: e.address,
+            //   latitude: e.latitude,
+            //   longitude: e.longitude
+            // })
+            _this.data.newAddress = [{
               id: new Date().getTime(),
               name: e.name,
               address: e.address,
               latitude: e.latitude,
               longitude: e.longitude
-            })
+            }];
+
             _this.setData({
               newAddress: _this.data.newAddress
             })
