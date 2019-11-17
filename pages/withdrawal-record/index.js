7423b99183e15e8e317dfcca7c2585cde25f3c01
goodliang <swl8806@126.com>
2019-11-17 22:09:30 +0800

    record

---
+++ pages/withdrawal-record/index.js
@@ -0,0 +1,104 @@
+const util = require('../../utils/util')
+
+function fmtDate(obj) {
+    var date = new Date(obj);
+    var y = 1900 + date.getYear();
+    var m = "0" + (date.getMonth() + 1);
+    var d = "0" + date.getDate();
+    return y + "-" + m.substring(m.length - 2, m.length);
+}
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        info: '',
+        loading: false,
+        dateModal: false,
+        dateText: fmtDate(new Date().getTime()),
+        minDate: new Date(2019, 5).getTime(),
+        currentDate: new Date().getTime(),
+        maxDate: new Date().getTime()
+    },
+    handleDateModal() {
+        this.setData({
+            dateModal: !this.data.dateModal
+        });
+    },
+    handleDate(event) {
+        this.setData({
+            currentDate: event.detail,
+            dateText: fmtDate(event.detail),
+            dateModal: !this.data.dateModal
+        });
+        this.getData()
+    },
+    getData() {
+        this.setData({
+            loading: true
+        })
+        util.wx.get('/api/seller/income_records', { ymonth: fmtDate(this.data.currentDate) })
+            .then(res => {
+                this.setData({
+                    loading: false
+                })
+                if (res.data.code == 200) {
+                    this.setData({
+                        info: res.data.data.datas
+                    })
+                    console.log('info',this.data.info)
+                }
+               
+            })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.getData()
+        console.log(this.data.currentDate)
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    }
+})
