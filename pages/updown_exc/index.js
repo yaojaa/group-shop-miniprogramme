4044746decc57f9d29b97210633b133450741bb4
shuxingxu <905336308@qq.com>
2019-12-12 17:54:32 +0800

    x

--- pages/updown_exc/index.js
+++ pages/updown_exc/index.js
@@ -18,7 +18,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.data.goods_id = options.id;
+    this.data.goods_id = options.goods_id;
     let t = util.formatTime(new Date());
     t = t.split(' ')
 
