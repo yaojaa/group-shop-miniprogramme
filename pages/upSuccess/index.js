92256b6c6ca0896be82feeb757dab0793ee7692e
goodliang <swl8806@126.com>
2020-03-19 13:42:18 +0800

    success

--- pages/upSuccess/index.js
+++ pages/upSuccess/index.js
@@ -7,21 +7,21 @@ Page({
      * 页面的初始数据
      */
     data: {
-        info:{}
+        info: {}
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
         this.data.goods_id = options.goods_id
         this.getGoodsInfo();
 
-       
+
 
     },
- 
-    
+
+
     goback() {
 
         wx.redirectTo({
@@ -29,54 +29,45 @@ Page({
         })
 
     },
-    toDetail(){
+    toDetail() {
 
-         wx.redirectTo({
-            url: '../goods/goods?goods_id='+this.data.goods_id
+        wx.redirectTo({
+            url: '../goods/goods?goods_id=' + this.data.goods_id
         })
 
 
     },
-    getGoodsInfo(){
+    getGoodsInfo() {
 
-          util.wx.get('/api/goods/get_goods_detail', {
-                goods_id: this.data.goods_id 
+        util.wx.get('/api/goods/get_goods_detail', {
+                goods_id: this.data.goods_id
             })
             .then(res => {
 
                 this.setData({
-                    info:res.data.data.goods
+                    info: res.data.data.goods
                 })
-              
+
             })
     },
     // addListener:util.userListner,
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function(res) {
-
-        this.setData({
-            clickShare:true,
-            onshare:true
-        })
-
-
-
-        let shareTitle = this.data.wordArr[this.data.create_number] || '大家再接再厉...' + this.data.goods_name
-        let numberIcon = ''
+    onShareAppMessage: function (res) {
         return {
-            title: '',
+            title: this.data.info.goods_name,
+            imageUrl:this.data.info.goods_cover,
             path: '/pages/goods/goods?goods_id=' + this.data.goods_id
         }
     },
-    formSubmit: function(e) {
+    formSubmit: function (e) {
         util.formSubmitCollectFormId.call(this, e)
     },
-    addListener:function () {
-          wx.requestSubscribeMessage({
-              tmplIds: ['17y_mLplxTn0resiR34oUsJMZu2E2W6i0x2YIRZgvZ4','Wu_vie78kgoRr8y90IAsxoEn87BJ3nDrEBLP0MK6208'],
-              success (res) { }
-            })
+    addListener: function () {
+        wx.requestSubscribeMessage({
+            tmplIds: ['17y_mLplxTn0resiR34oUsJMZu2E2W6i0x2YIRZgvZ4', 'Wu_vie78kgoRr8y90IAsxoEn87BJ3nDrEBLP0MK6208'],
+            success(res) {}
+        })
     }
 })
