502396e0b9f3ca0b0f5d849431640c95d28a7500
goodliang <swl8806@126.com>
2020-03-20 23:15:26 +0800

    11

--- pages/supplier-home/index.js
+++ pages/supplier-home/index.js
@@ -2,54 +2,48 @@ const util = require('../../utils/util.js')
 import data from '../../utils/city_data'
 import { $wuxToptips } from '../../wux/index'
 
-
-
-
 Page({
-
   /**
    * 页面的初始数据
    */
-   data: {
-        goodsList:[],
-        info:{}
-    },
-   bindRegionChange(e) {
-
+  data: {
+    goodsList: [],
+    info: {}
   },
-  getSuppInfo(){
-
-       util.wx.get('/api/seller/get_supplier_detail?supplier_id='+this.id).then(res=>{
-      this.setData({
-        info:res.data.data
-      })
-    })
-  },
-  getgoodsInfo(){
-
-    util.wx.get('/api/seller/get_supplier_goods?supplier_id='+this.id).then(res=>{
-      this.setData({
-        goodsList:res.data.data.list
-      })
-    })
-  },
-
-    goods_up(e){
-
-
-        console.log('go',e)
-
-        const {id} = e.target.dataset
-
-
-        wx.navigateTo({
-            url: '/pages/goods-up/index?supid=' + id
+  bindRegionChange(e) {},
+  getSuppInfo() {
+    util.wx
+      .get('/api/seller/get_supplier_detail?supplier_id=' + 6)
+      .then(res => {
+        this.setData({
+          info: res.data.data
         })
+        wx.setNavigationBarTitle({
+          title: this.data.info.supplier_name
+        }) 
+      })
+  },
+  getgoodsInfo() {
+    util.wx
+      .get('/api/seller/get_supplier_goods?supplier_id=' + this.id)
+      .then(res => {
+        this.setData({
+          goodsList: res.data.data.list
+        })
+      })
+  },
 
-    },
+  goods_up(e) {
+    console.log('go', e)
 
-  onLoad(option){
+    const { id } = e.target.dataset
 
+    wx.navigateTo({
+      url: '/pages/goods-up/index?supid=' + id
+    })
+  },
+
+  onLoad(option) {
     this.id = option.id
 
     this.getSuppInfo()
@@ -59,42 +53,30 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  }
+  onReachBottom: function() {}
 })
