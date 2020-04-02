262319dcb14fc3774aa3eaf66f39dadbde402535
yaojia <yaojia@qq.com>
2020-04-02 22:16:37 +0800

    add

--- business/components/footerbar/index.js
+++ business/components/footerbar/index.js
@@ -1,64 +1,64 @@
-
 Component({
-    externalClasses: ['custom-class'],
-    properties: {
-        active: {
-            type: Number,
-            //wait、process、finish、error
-            value: 0 
-          }
-    },
-    options: {},
-    relations: {
-
-    },
-    data: {
-      is_recommend:1,
-      expires: '',
-        urls: [
-           
-        ]
-    },
-    lifetimes: {
-      attached: function() {
-
-      },
-      detached: function() {
-        // 在组件实例被从页面节点树移除时执行
-      }
-   },
-    methods: {
-
-          goActing() {
-          wx.navigateTo({
-            url: '../../pages/acting-admin/index'
-            })
-            },
-            goMy(){
-               wx.redirectTo({
-                    url: '../../pages/setting-list/index'
-                })
-
-            },
-
-            goOrder(){
-               wx.navigateTo({
-                    url: '../../pages/order-manage/index'
-                })
-
-            },
-            goHome() {
-
-                wx.redirectTo({
-                    url: '../../pages/home/index'
-                })
-            },
-
-            onChange(event) {
-              // event.detail 的值为当前选中项的索引
-              this.setData({ active: event.detail });
-            }
-    },
-    ready() {
+  externalClasses: ['custom-class'],
+  properties: {
+    active: {
+      type: Number,
+      //wait、process、finish、error
+      value: 0
     }
+  },
+  options: {},
+  relations: {
+
+  },
+  data: {
+    is_recommend: 1,
+    expires: '',
+    urls: [
+
+    ]
+  },
+  lifetimes: {
+    attached: function () {
+
+    },
+    detached: function () {
+      // 在组件实例被从页面节点树移除时执行
+    }
+  },
+  methods: {
+
+    goActing() {
+      wx.redirectTo({
+        url: '../../pages/acting-admin/index'
+      })
+    },
+    goMy() {
+      wx.redirectTo({
+        url: '../../pages/setting-list/index'
+      })
+
+    },
+
+    goOrder() {
+      wx.redirectTo({
+        url: '../../pages/order-manage/index'
+      })
+
+    },
+    goHome() {
+
+      wx.redirectTo({
+        url: '../../pages/home/index'
+      })
+    },
+
+    onChange(event) {
+      // event.detail 的值为当前选中项的索引
+      this.setData({
+        active: event.detail
+      });
+    }
+  },
+  ready() {}
 })
