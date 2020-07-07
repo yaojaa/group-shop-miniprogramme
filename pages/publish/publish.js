9073d08e162a4b60c2b0bfa22e93bec07a7674e5
yaojia <yaojia@qq.com>
2020-07-07 22:50:27 +0800

    add

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -732,6 +732,9 @@ Page({
   },
   //提交表单
   submitForm(e) {
+
+    console.log(e.detail.value)
+
     this.initValidate()
 
     if (this.data.goods_images.length <= 0) {
@@ -837,16 +840,27 @@ Page({
   saveEdit(){
 
 
-    console.log(JSON.stringify(this.data.spec), JSON.stringify(this.oldSpec))
+    console.log(JSON.stringify(this.data.spec) == JSON.stringify(this.data.oldSpec))
 
-    if(JSON.stringify(this.data.spec) ===  JSON.stringify(this.oldSpec)){
+    console.log(this.data.spec)
+        console.log(this.data.oldSpec)
+
+
+    if(JSON.stringify(this.data.spec) == JSON.stringify(this.data.oldSpec)){
+      this.jump()
       return
     }
     
 
-
-    this.editor({
-      spec:this.data.spec
+    util.wx.post('/api/seller/goods_add_or_edit',Object.assign({goods_id:this.data.goods_id,spec:this.data.spec})).then(res=>{
+      if(res.data.code !== 200){
+        wx.showToast({
+          title:res.data.msg,
+          icon:'none'
+        })
+      }else{
+        this.jump()
+      }
     })
 
   },
@@ -974,9 +988,8 @@ Page({
         let d = res.data
         let gs = d.data.goods
 
-        this.oldSpec = gs.spec
-
         if (d.code == 200) {
+          this.data.oldSpec = Object.assign({},gs.goods_spec)
           // 初始数据
           this.initData(gs, isCopy)
         } else {
@@ -1045,7 +1058,7 @@ Page({
 
     content_imgs:(newValue, val, context)=>{
 
- if(newValue == val){
+    if(newValue == val){
         return
       }
       context.editor({content_imgs:newValue})
