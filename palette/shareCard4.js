853ac8a04bfff7b2bdd723dfcb0fca0824ba89c5
shuxing <905336308@qq.com>
2020-08-05 07:47:09 +0800

    x

--- palette/shareCard4.js
+++ palette/shareCard4.js
@@ -14,27 +14,45 @@ const hei = 280;
 function arr(goods){
 console.log(goods.goods_cover)
   return [
-    // 相片
-    {
-      type: 'image',
-      url: goods.goods_cover,
-      css: {
-        top: `50px`,
-        left: `120px`,
-        width: '110px',
-        height: '100px',
-      },
-    },
     //绘制背景边框
     {
       type: 'image',
-      url: '/palette/share_side.png',
+      url: '/palette/share_side.jpg',
       css: {
         top: `0px`,
         left: `0px`,
         width: `${wid}px`,
         height: `${hei}px`,
       },
+    },
+    // 相片
+    {
+      type: 'image',
+      url: goods.goods_cover,
+      css: {
+        top: `55px`,
+        left: `${wid/2}px`,
+        width: '130px',
+        height: '120px',
+        align: 'center',
+        borderRadius: '8px',
+        borderWidth: '4px',
+        borderColor: '#999',
+      },
+    },
+
+    {
+      type: 'text',
+      text: '#绑定商品#',
+      css: {
+        top: `205px`,
+        left: `${wid/2}px`,
+        color: '#666666',
+        fontSize: "24px",
+        lineHeight: '60px',
+        fontWeight: 'bold',
+        align: 'center'
+      }
     },
   ];
 }
