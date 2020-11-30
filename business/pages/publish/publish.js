afd31b541733047fc590e261cbf862a2a9851ecf
shuxing <905336308@qq.com>
2020-11-30 21:15:16 +0800

    x

--- business/pages/publish/publish.js
+++ business/pages/publish/publish.js
@@ -101,7 +101,8 @@ Page({
     is_on_sale_status: true,
     freight_tpl_id: 0, //运费模版ID 默认0
     freight_tpl_name: '默认方案全国包邮', //运费模版ID 默认0
-    editorContent: null  
+    editorContent: null,
+    currentScrollTop: 0
   },
 
   sale_status_onChange(event) {
@@ -965,6 +966,11 @@ Page({
       // 是否是模板
       this.getPublishedData('', '', option.temp)
     }
+  },
+  onPageScroll(e){
+    this.setData({
+      currentScrollTop: e.scrollTop
+    });
   },
   inputDuplex: util.inputDuplex
 })
